2b574911f63a7f9b7ee00bc6baf4f8f73403cdea
blimbu1 <ranger09@gmail.com>
2017-11-19 22:04:16 +0000

    Permuations code complete

--- Permutations/Permute.py
+++ Permutations/Permute.py
@@ -2,7 +2,15 @@ text = 'Hello'
 def permutations(text):
 	j = []
 	j.append(text)
-	y = list(text)
-	print(y)
+	b = text
+	for i in range(0, len(text)):
+		a = b[0]
+		c = b[1:]
+		for y in range(0,len(c)):
+			s=c[0:y+1] + a + c[y+1:]
+			if s not in j:
+				j.append(s)
+		b = c+a
+	print(j)			
 
 permutations(text)
